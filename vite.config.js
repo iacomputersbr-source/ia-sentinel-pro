0472a8f301263431b2765754cb710d24bf3b10ca
IA Computers <iacomputersbr@gmail.com>
2026-09-24 12:49:48 -0300

    Add initial Vite configuration file

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+export default defineConfig({
+  clearScreen: false,
+  server: { port: 1420, strictPort: true },
+  envPrefix: ["VITE_", "TAURI_"],
+  build: {
+    target: "chrome105",
+    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+    sourcemap: !!process.env.TAURI_DEBUG
+  }
+});
